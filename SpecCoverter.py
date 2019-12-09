25d37d81a056acae3a6449effef3eb1e54874a93
younghokim <graycoral.kim@gmail.com>
2019-12-10 00:57:11 +0900

    [SpecCoverter] Make class

--- SpecCoverter.py
+++ SpecCoverter.py
@@ -1,15 +1,18 @@
+import sys
 import PyPDF2
+import textract
 
-PDFfilename = "AUTOSAR_PRS_SOMEIPServiceDiscoveryProtocol.pdf" #filename of your PDF/directory where your PDF is stored
 
-pfr = PyPDF2.PdfFileReader(open(PDFfilename, "rb")) #PdfFileReader object
+class SpecConvert:
+    pfr = ""
+    def __init__(self, PDFfilename):
+        self.pfr = PyPDF2.PdfFileReader(open(PDFfilename, "rb"))  # PdfFileReader object
 
-pg4 = pfr.getPage(126) #extract pg 127
+    def openPdf():
+        number_of_pages = pfr.getNumPages()
+        c = collections.Counter(range(number_of_pages))
+        for i in c:
+            page = pfr.getPage(i)
+            page_content = page.extractText()
+            print page_content.encode('utf-8')
 
-writer = PyPDF2.PdfFileWriter() #create PdfFileWriter object
-#add pages
-writer.addPage(pg4)
-
-NewPDFfilename = "allTables.pdf" #filename of your PDF/directory where you want your new PDF to be
-with open(NewPDFfilename, "wb") as outputStream:
-    writer.write(outputStream) #write pages to new PDF
